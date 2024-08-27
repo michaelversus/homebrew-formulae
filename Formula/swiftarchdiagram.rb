a5fb0f42c3ac322f6a58bf2150655046dc242331
Michael <michaliskarag@gmail.com>
2024-08-27 20:28:01 +0300

    Add files via upload

---
+++ Formula/swiftarchdiagram.rb
@@ -0,0 +1,16 @@
+class Swiftarchdiagram < Formula
+  desc "Swift Architecture diagram generation using mermaid"
+  homepage "https://github.com/michaelversus/SwiftArchDiagram"
+  url "https://github.com/michaelversus/SwiftArchDiagram.git", tag: "0.1.3"
+  version "0.1.3"
+
+  depends_on "xcode": [:build]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "#{bin}SwiftArchDiagram", "list"
+  end
+end
