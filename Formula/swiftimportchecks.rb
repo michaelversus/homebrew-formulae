4f76eace4cf23272903f652aa0bfb24be401cd88
Michael <michaliskarag@gmail.com>
2025-05-03 20:08:39 +0300

    Update swiftimportchecks.rb

--- Formula/swiftimportchecks.rb
+++ Formula/swiftimportchecks.rb
@@ -1,8 +1,8 @@
 class Swiftimportchecks < Formula
   desc "Swift implicit import checks for SPM packages and Xcodeproj"
   homepage "https://github.com/michaelversus/SwiftImportChecks"
-  url "https://github.com/michaelversus/SwiftImportChecks.git", tag: "0.1.0"
-  version "0.1.0"
+  url "https://github.com/michaelversus/SwiftImportChecks.git", tag: "0.1.1"
+  version "0.1.1"
 
   depends_on "xcode": [:build]
 
