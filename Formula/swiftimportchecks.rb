9874592b45031378d6dfb6ccead3b9f201092d07
Michael <michaliskarag@gmail.com>
2025-05-03 19:46:41 +0300

    Create swiftimportchecks.rb

---
+++ Formula/swiftimportchecks.rb
@@ -0,0 +1,16 @@
+class Swiftimportchecks < Formula
+  desc "Swift implicit import checks for SPM packages and Xcodeproj"
+  homepage "https://github.com/michaelversus/SwiftImportChecks"
+  url "https://github.com/michaelversus/SwiftImportChecks.git", tag: "0.1.0"
+  version "0.1.0"
+
+  depends_on "xcode": [:build]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "#{bin}SwiftImportChecks", "list"
+  end
+end
